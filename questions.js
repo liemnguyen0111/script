de9170adb7933502a5069358991b8c5bea73209c
Liem Nguyen <liemnguyen0111@gmail.com>
2020-04-29 02:42:24 -0700

    Added script file

---
+++ questions.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        question: 'What is the correct way to write an if-else statement?',
+        choices: 
+        ['If(condition){ //do something } else(condition){ //do something}',
+        'If(condition){ //do something } else{ //do something}',
+        'All the above',
+        'None of the above'],
+        answer: 1
+    },
+    {
+        question: 'Given an integer i equal to 1, which of the following will make i equal 2',
+        choices: ['++i','i++','All of the above','None of the above'],
+        answer: 2
+    },
+    {
+        question: 'Which will allow you to convert a string to a number in JavaScript?',
+        choices: ['parseInt()','===','intParse()','JSON.stringify()'],
+        answer: 0
+    },
+    {
+        question: 'Which below is the correct way to write a function?',
+        choices: ['const hotdog () {}','const hotdog = function ()','const hotdog = () => {}','All of the above'],
+        answer: 2
+    },
+    {
+        question: `What would be the the result of 4 + 8 + '2'?`,
+        choices: ['14','122','410','6'],
+        answer: 1
+    },
+    {
+        question: `NaN stands for: `,
+        choices: [`Numbers-aNonymous`, `Not-any-Nannies`, `Not-a-Number`, `No-absolutely-Not`],
+        answer: 2
+    },
+    {
+        question: `An array is enclosed in: `,
+        choices: [`'quotes'`, `{curly brackets}`, `(parentheses)`, `[square brackets]`],
+        answer: 3
+    },
+    {
+        question: `Terminates the current loop and goes to the statement following the current statement: `,
+        choices: [`yeet`, `break`, `continue`, `stop`],
+        answer: 1
+    },
+    {
+        question: `The greater than/or equal operator is: `,
+        choices: [`>>`, `<=`, `+=`, `>=`],
+        answer: 3
+    },
+    {
+        question: `This is a value that is passed back from one function to another: `,
+        choices: [`return`, `class`, `functionClass`, `for`],
+        answer: 0
+    },
+    {
+        question: `Which of the following function of an array object adds one or more elements to the front of an array and returns the new length of the array?`,
+        choices: [`unshift()`, `sort()`, `splice()`, `toString()`],
+        answer: 0
+    },
+    {
+        question: `Which built-in method adds one or more elements to the end of an array and returns the new length of the array?`,
+        choices: [`last()`, `put()`, `push()`, `pop()`],
+        answer: 2
+    },
+    {
+        question: ` Which built-in method returns the characters in a string beginning at the specified location?`,
+        choices: [`substr()`, `getSubstring()`, `slice()`, `None of the above.`],
+        answer: 1
+    },
+    {
+        question: `Which of the following function of an array object adds and/or removes elements from an array?`,
+        choices: [`toSource()`, `sort()`, `unshift()`, `splice()`],
+        answer: 3
+    },
+    {
+        question: `Which of the following function of String object combines the text of two strings and returns a new string?`,
+        choices: [`add()`, `concat()`, ` merge()`, `append()`],
+        answer: 1
+    }
+  
+]
